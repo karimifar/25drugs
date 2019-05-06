e94e604097029e753491e6370fe1138a2761b76c
Karimifar <em.karimifar@gmail.com>
2019-05-06 16:43:44 -0500

    closing day

---
+++ assets/js/content.js
@@ -0,0 +1,149 @@
+
+
+var content = [
+    {
+        num:22,
+        title: "New Hope for Fighting Orphan Cancers",
+        shortTitle: "Orphan Cancers",
+        subhead: "Researchers have discovered that malignant rhabdoid tumors (MRT), a rare pediatric cancer without effective treatments, may be sensitive to drugs that block the cancer cell’s ability to dispose of misfolded proteins. The findings provide a much-needed therapeutic target for these and other cancers caused by mutations in the SMARCB1 gene. Based on these findings, the researchers now are leading a Phase II clinical trial to investigate this approach in renal medullary carcinoma (RMC), a related adolescent cancer also characterized by SMARCB1 mutations. Malignant rhabdoid tumors are aggressive pediatric cancers that generally manifest in children in the first year of life.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "MD Anderson",
+        Link: "https://www.mdanderson.org/newsroom/new-therapeutic-target-found-for-aggressive-pediatric-cancers-with-few-treatment-options.h00-159300678.html",
+        matter:true,
+    },
+
+    {
+        num:20,
+        title: "Double-drug Strategy Blocks Escape Route for Most Lung Cancers",
+        shortTitle: "One-Two Punching Lung Cancer",
+        subhead: "A one-two combo punch using two currently available drugs could be an effective treatment for the majority of lung cancers, a study by scientists with UT Southwestern’s Simmons Cancer Center shows. Researchers found that a combination of drugs, one targeting epidermal growth factor receptor (EGFR) and one targeting tumor necrosis factor (TNF), effectively blocks the cancer from using TNF as an escape route. Using a mouse model, the researchers showed that when TNF is also blocked, the cancer becomes sensitive to EGFR treatment. The latest findings build on previous work showing that the same combination of drugs was successful in a mouse model of glioblastoma, a deadly type of brain cancer.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UT Southwestern",
+        Link: "https://www.utsouthwestern.edu/newsroom/articles/year-2018/lung-cancer-habib.html",
+        matter:true,
+    },
+
+    {
+        num:23,
+        title: "Toward a Cure for the Kissing Bug Disease?",
+        shortTitle: "New Chagas Hope",
+        subhead: "Chagas disease is a potentially life-threatening disease caused by the parasite Trypanosoma cruzi, which is transmitted to animals and people by insect vectors, popularly known as kissing bugs, and by blood transfusion, organ transplant, congenitally, and by contaminated foods and juices. The disease has been endemic to Latin America, affecting 6 million to 7 million people, but it is rapidly spreading through the United States, Europe and other nonendemic regions as a result of globalization. Yet, there is no fully effective drug and no clinical vaccine. Researchers at UTEP will conduct a phase II clinical trial in Bolivia with new regimens of the drugs benznidazole and nifurtimox.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UTEP",
+        Link: "https://www.utep.edu/newsfeed/utep-scientists-awarded-6m-to-improve-treatment-for-chagas-disease.html",
+        matter:true,
+    },
+
+    {
+        num:24,
+        title: "Nanodiamond Based Anti-HIV Drug Delivery Targeted Towards the Brain.”",
+        shortTitle: "Nanodiamonds Against HIV",
+        subhead: "To further support the study of human immunodeficiency virus 1 (HIV-1), which is still one of the leading causes of death worldwide, the National Institutes of Health (NIH) recently awarded a grant to UTRGV researchers for their project, called “Nanodiamond Based Anti-HIV Drug Delivery Targeted Towards the Brain.” The proposed research develops a molecule that will be conjugated with the microglia-specific antibody for targeted drug delivery to the brain. Microglia is a type of cell that is majorly found throughout the brain. The proposed nanodrug can be used to directly deliver an anti-HIV drug specifically to the inflammatory areas of the brain where most of the infected microglia and the virus are present. ",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UTRGV",
+        Link: "https://www.utrgv.edu/en-us/about-utrgv/news/press-releases/2018/august-03-utrgv-awarded-grant-for-anti-hiv-drug-research/index.htm",
+        matter:true,
+    },
+
+    {
+        num:13,
+        title: "Are Statins the Answer to Preeclampsia?",
+        shortTitle: "The New/Old Pre-e Drug",
+        subhead: "Researchers have been working for decades, with little success, on ways to treat preeclampsia or reduce the risk of developing it. They’ve tried blood pressure medications, antioxidant vitamins, salt restriction, aspirin, fish oil, and calcium, all with little or no effect. A new drug that dramatically reduced the risks would be a unicorn under any circumstances. That the new drug might in fact be an old drug, proven safe in many studies and already sold cheaply as a generic, would be extraordinary. ",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "The University of Texas Medical Branch",
+        link: "",
+        matter:true,
+    },
+
+    {
+        num:15,
+        title: "Dr. Bubble Popper",
+        shortTitle: "Dr. Bubble Popper",
+        subhead: "“We can cure almost anything,” says Dr. David McPherson. “Any cancer. Any heart disease. But the quantities needed to do so, right now, may kill the patient.” This is true, he says, not just of injectable drugs, but of pills, inhalants, and every other method of drug delivery. What’s needed are new, better, more universal ways to get drugs where we need them and keep them away from where we don’t. McPherson’s solution, on which he’s been working for almost 20 years, is a class of carriers called echogenic liposomes. They are essentially tiny innerspace submarines, with an outer lipid shell, that carry different types of drugs throughout the bloodstream.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UTHealth",
+        link: "http://www.utsystempophealth.org/a-solution-to-the-flu-problem",
+        matter:true,
+    },
+
+    {
+        num:16,
+        title: "Anti-Alcoholism Drug Shows Promise in Animal Models",
+        shortTitle: "Anti-Alcoholism Drug",
+        subhead: "Scientists at The University of Texas at Austin have successfully tested in animals a drug that, they say, may one day help block the withdrawal symptoms and cravings that incessantly coax people with alcoholism to drink. If eventually brought to market, it could help the more than 15 million Americans, and many more around the world who suffer from alcoholism stay sober. If what has been shown to work in worms and rats addicted to alcohol can eventually be demonstrated to work in humans with minimal side effects, it would be a true breakthrough.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UT Austin",
+        Link: "https://news.utexas.edu/2018/05/03/anti-alcoholism-drug-shows-promise-in-animal-models/",
+        matter:true,
+    },
+
+    {
+        num:17,
+        title: "Biologists Design New Molecules To Help Stall Lung Cancer",
+        shortTitle: "A Stalling Lung Cancer",
+        subhead: "In a preclinical study, biologist Dr. Li Zhang and her team showed that the expansion of lung tumors in mice slowed when access to heme — the oxygen-binding molecule in hemoglobin — was restricted. The researchers also showed that those same cancers grew faster when more heme was available than normal. With an eye toward exploiting the cancer’s heme dependency, two of Zhang’s graduate students then engineered and extensively characterized new molecules aimed at starving the cancer cells of the molecule that allows them to proliferate so quickly.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UT Dallas",
+        Link: "https://www.utdallas.edu/news/research/university-biologists-design-new-molecules-to-help-stall-lung-cancer/",
+        matter:true,
+    },
+
+    {
+        num:18,
+        title: "The Cutting Edge",
+        shortTitle: "The Cutting Edge",
+        subhead: "Ben Dupree peered through the microscope at the beating heart cells, awestruck by the implications. These were his cells, edited in the lab to correct a genetic flaw that had caused his muscles to deteriorate since childhood, forced him into a wheelchair, and perpetuated the prospects of an early death from Duchenne muscular dystrophy (DMD. Yet, here he witnessed a breakthrough that changed the narrative, a realization that after years of despair and depression, a lifesaving treatment for his condition was no longer a far-fetched dream.“It’s unbelievable,” Mr. Dupree, 26, said as he looked at his cells during a recent visit to a UT Southwestern gene-editing lab. “This could save so many lives one day.”",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UT Southwestern",
+        Link: "https://www.utsouthwestern.edu/newsroom/articles/year-2019/cutting-edge.html",
+        matter:true,
+    },
+
+    {
+        num:17,
+        title: "Biologists Design New Molecules To Help Stall Lung Cancer",
+        shortTitle: "Stalling Lung Cancer",
+        subhead: "In a preclinical study, biologist Dr. Li Zhang and her team showed that the expansion of lung tumors in mice slowed when access to heme — the oxygen-binding molecule in hemoglobin — was restricted. The researchers also showed that those same cancers grew faster when more heme was available than normal. With an eye toward exploiting the cancer’s heme dependency, two of Zhang’s graduate students then engineered and extensively characterized new molecules aimed at starving the cancer cells of the molecule that allows them to proliferate so quickly.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UT Dallas",
+        Link: "https://www.utdallas.edu/news/research/university-biologists-design-new-molecules-to-help-stall-lung-cancer/",
+        matter:true,
+    },
+
+    {
+        num:21,
+        title: "From the Bedside to the Bench and Back Again",
+        shortTitle: "Need-Driven Drug Development",
+        subhead: "”We are completely driven by unmet needs we see in the patients that come to MD Anderson for help,” says Phil Jones, Ph.D., vice president of Therapeutics Discovery. ”Guided by the expertise of our world-class clinicians, our efforts begin with the patient and their cancer. The Therapeutics Discovery model is designed to develop new treatments to meet their needs. Unlike typical pharmaceutical companies, Therapeutics Discovery was built within the walls of MD Anderson, placing drug development expertise in unparalleled proximity to patients and leading physicians. It's a recipe for success that already is yielding promising results. And at the heart of it all are more than 100 scientists, driven by a passion to see their work one day save a patient's life.”",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "MD Anderson",
+        Link: "https://www.mdanderson.org/publications/conquest/iacs-and-other-platforms-develop-new-cancer-treatments-for-clinical-trials.h36-1592202.html",
+        matter:true,
+    },
+
+    {
+        num:19,
+        title: "Children of Hope",
+        shortTitle: "Children of Hope",
+        subhead: "Chrissy Grube’s face tensed with effort. She took one laborious step in her walker, then another, pausing for breath before resuming the slow trek from the kitchen. “I am so happy,” her younger sister chimed quietly in a sing-song voice, her shoes clunking on the wooden floor as she hopped excitedly next to their father a few feet ahead. This was a big moment for the sisters. Both had been diagnosed with a rare genetic disease that was slowly killing 10-year-old Chrissy and would next take aim at Amanda. Several months prior Chrissy could not even wiggle a toe, much less stand up – the rapid deterioration of her nervous system forcing her into a wheelchair by age 8. Then, a marvel of medicine.",
+        illustration: "assets/img/illustrations/tbi.png",
+        instLogo: "assets/img/inst-logos/utmb.png",
+        institution: "UT Southwestern",
+        Link: "https://www.utsouthwestern.edu/newsroom/articles/year-2018/children-of-hope.html",
+        matter:true,
+    }
+]
+
+
